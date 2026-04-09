7d4c25620e7537bc85aae926722ed9f3c1574395
David Garcia-Martinez <joshuagarcia_12@yahoo.com>
2026-04-09 11:17:24 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by GARCIA-MARTINEZ, DAVID on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
